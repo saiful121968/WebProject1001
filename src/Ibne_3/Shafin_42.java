c4fbd21f168e71b8277e09d298b0da249c0da3c3
saiful <saiful121968@gmail.com>
2018-01-06 08:42:42 -0500

    new file

---
+++ src/Ibne_3/Shafin_42.java
@@ -0,0 +1,21 @@
+package Ibne_3;
+
+public class Shafin_42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int x=30, y=45;
+		
+		boolean z ; 
+		z=y<x;
+		
+		System.out.println("x is greater than y=\t"+ z);
+		
+		
+	 z=y>x;
+		System.out.println("y is greater than x=\t"+ z);
+
+	}
+
+}
