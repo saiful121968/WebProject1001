ffa9cd2af9132a551b4180f667010a4711292abb
saiful <saiful121968@gmail.com>
2018-01-06 08:47:22 -0500

    file added

---
+++ src/Ibne_3/Shafin_43.java
@@ -0,0 +1,18 @@
+package Ibne_3;
+
+public class Shafin_43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int Mark[] = {23, 54, 50, 45, 65};
+		
+		
+		for (int i = 0; i<10; i++){
+			System.out.println("Mark[" + i + "] = " +Mark[i]);	
+			 
+
+	}
+
+}
+}
