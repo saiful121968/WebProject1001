546c9a9f4a273c333c0992df39a672186ec513b6
saiful <saiful121968@gmail.com>
2018-01-06 11:35:19 -0500

    Using clone

---
+++ src/Ibne_8/Shafin_52.java
@@ -0,0 +1,28 @@
+package Ibne_8;
+import java.util.*;
+// Using 
+public class Shafin_52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> b1 = new ArrayList<String>();
+
+        // use add() method to add elements in the list
+
+        b1.add("A");
+        b1.add("B");
+        b1.add("C");
+        b1.add("D");
+        
+        System.out.println(b1);
+
+        //Using clone() method to copy b1 into a new b1Copy Arraylist
+
+        ArrayList<String> b1Copy = (ArrayList<String>) b1.clone();
+
+        System.out.println(" b1 Copy elements copied from b1\n "+b1Copy);
+
+	}
+
+}
