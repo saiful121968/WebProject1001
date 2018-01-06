0b79c759278c7f95161b43523295ab2dad915339
saiful <saiful121968@gmail.com>
2018-01-06 08:56:18 -0500

    file added

---
+++ src/Ibne_3/Shafin_45.java
@@ -0,0 +1,18 @@
+package Ibne_3;
+
+public class Shafin_45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Roll[]; float Mark[];
+		   Roll= new int [5]; Mark= new float [5];
+		   int size=Mark.length; 
+		   
+		   for (int i=0; i<size; ++i){
+			   System.out.println("Roll["+i +"] is ="+Roll[i]);
+			   System.out.println("\t Mark["+i +"] is ="+Mark[i]);}
+
+	}
+
+}
