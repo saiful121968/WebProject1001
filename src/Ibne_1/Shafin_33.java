0c612ca40aba608d0ce62021198e4db1dc9699d8
saiful <saiful121968@gmail.com>
2018-01-06 07:23:01 -0500

    This is my first programm to this project

---
+++ src/Ibne_1/Shafin_33.java
@@ -0,0 +1,12 @@
+package Ibne_1;
+
+public class Shafin_33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Assalamu Alaikum !\nThis is my first project !\nRanzu is my nick name !");
+
+	}
+
+}
