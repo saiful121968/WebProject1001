03b4f2c1379a1046cd1565ef18193fc29e18fe4b
saiful <saiful121968@gmail.com>
2018-01-05 19:31:51 -0500

    File shafin_31 added

---
+++ src/Ibne_8/Shafin_31.java
@@ -0,0 +1,25 @@
+package Ibne_8;
+
+public class Shafin_31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Integer   I = new Integer(350) ;
+		Float     F = new Float(35.0) ;
+		Character C = new Character('A') ;
+		Boolean   B = new Boolean(true) ;
+
+		int  i = I.intValue();;
+		float  f = F.floatValue();
+		System.out.println("I = " + I + "\t\t i = " + i);
+		System.out.println("F = " + F + "\t f = " + f);
+		char c = C.charValue();
+		boolean b  = B.booleanValue();
+		System.out.println("C = " + C + "\t\t c = " + c);
+		System.out.println("B = " + B + "\t b = " + b);
+
+	}
+
+}
